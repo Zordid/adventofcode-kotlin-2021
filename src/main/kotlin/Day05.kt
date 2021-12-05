a1af78ded40a936278542191ff24ad53b6dbaf8f
Olaf Gottschalk <Zordid@users.noreply.github.com>
2021-12-05 07:28:42 +0100

    Solved day 5

---
+++ src/main/kotlin/Day05.kt
@@ -0,0 +1,45 @@
+import utils.Direction8
+import utils.Point
+import utils.minus
+import utils.plus
+
+class Day05 : Day(5, 2021, "Hydrothermal Venture") {
+
+    data class Line(val p1: Point, val p2: Point) {
+        private val orientation = Direction8.ofVector(p2 - p1)
+        val isHorizontalOrVertical =
+            orientation in listOf(Direction8.NORTH, Direction8.SOUTH, Direction8.EAST, Direction8.WEST)
+        val allPoints: List<Point> = sequence {
+            var x = p1
+            while (x != p2) yield(x).also { x += orientation!! }
+            yield(x)
+        }.toList()
+    }
+
+    private val lines: List<Line> =
+        mappedInput { it.extractAllIntegers().let { (x1, y1, x2, y2) -> Line((x1 to y1), (x2 to y2)) } }
+
+    override fun part1(): Int {
+        val hv = lines.filter { it.isHorizontalOrVertical }
+        return hv.flatMap { it.allPoints }.groupingBy { it }.eachCount().count { it.value > 1 }
+    }
+
+    override fun part2(): Int =
+        lines.flatMap { it.allPoints }.groupingBy { it }.eachCount().count { it.value > 1 }
+
+}
+
+fun main() {
+    solve<Day05>("""
+        0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2
+    """.trimIndent(), 5, 12)
+}
