96fca9230505da9f4a86d2ee1d8f94051836539a
Olaf Gottschalk <Zordid@users.noreply.github.com>
2021-12-07 07:21:28 +0100

    Adding visualization for day 5

---
+++ src/main/kotlin/Day05Graphical.kt
@@ -0,0 +1,37 @@
+import utils.*
+import java.awt.Color
+
+class Day05Graphical : PixelGameEngine() {
+
+    private val day05 = Day05()
+    private val lines = day05.lines
+
+    init {
+        val area = lines.flatten().boundingArea()!!
+
+        construct(area.width, area.height, 1, 1, day05.title)
+        limitFps = 100
+        println(area)
+    }
+
+    var line = 0
+
+    override fun onUpdate(elapsedTime: Long, frame: Long) {
+        if (line <= lines.lastIndex) {
+            val l = lines[line++]
+            drawLine(l.p1.x, l.p1.y, l.p2.x, l.p2.y)
+        } else if (line == lines.lastIndex + 1) {
+            val red = lines.flatMap { it.allPoints }.groupingBy { it }.eachCount().filter { it.value > 1 }.keys
+            red.forEach {
+                draw(it.x, it.y, Color.RED)
+            }
+            line++
+        } else
+            sleep(1000)
+    }
+
+}
+
+fun main() {
+    Day05Graphical().start()
+}
