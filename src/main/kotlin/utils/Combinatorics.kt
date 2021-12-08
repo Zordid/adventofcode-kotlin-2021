8d2aef7788166efb446187b86c559ec50918843c
Olaf Gottschalk <Zordid@users.noreply.github.com>
2021-12-08 17:18:41 +0100

    Add permutations fun

--- src/main/kotlin/utils/Combinatorics.kt
+++ src/main/kotlin/utils/Combinatorics.kt
@@ -9,13 +9,40 @@ package utils
 fun <T> List<T>.combinations(size: Int): Sequence<List<T>> =
     when (size) {
         0 -> emptySequence()
-        1 -> this@combinations.asSequence().map { listOf(it) }
+        1 -> asSequence().map { listOf(it) }
         else -> sequence {
             this@combinations.forEachIndexed { index, element ->
                 this@combinations.subList(index + 1, this@combinations.size).combinations(size - 1).forEach {
-                    yield(listOf(element) + it)
+                    yield(it.toMutableList().apply { add(0, element) })
                 }
             }
         }
     }
 
+/**
+ * Generates a sequence of all permutations of the given list of elements.
+ * @receiver the list of elements for permutation of order
+ * @return a sequence of all permutations of the given list
+ */
+fun <T> List<T>.permutations(): Sequence<List<T>> =
+    when (size) {
+        0 -> emptySequence()
+        1 -> sequenceOf(this)
+        else -> {
+            val first = first()
+            val remaining = subList(1, size)
+            sequence {
+                remaining.permutations().forEach { perm ->
+                    for (i in 0..perm.size) {
+                        yield(perm.toMutableList().apply { add(i, first) })
+                    }
+                }
+            }
+        }
+    }
+
+fun String.permutations(): Sequence<String> =
+    toList().permutations().map { it.joinToString("") }
+
+fun String.combinations(size: Int): Sequence<String> =
+    toList().combinations(size).map { it.joinToString("") }
--- src/test/kotlin/utils/CombinatoricsTest.kt
+++ src/test/kotlin/utils/CombinatoricsTest.kt
@@ -1,42 +1,51 @@
 package utils
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import org.junit.jupiter.api.Test
 
 internal class CombinatoricsTest {
 
     @Test
-    fun simpleCombinationsOf2() {
-        assertEquals(
-            emptyList<Int>(),
-            listOf(1).combinations(2).toList()
-        )
-        assertEquals(
-            listOf(
-                listOf(1, 2),
-            ),
-            listOf(1, 2).combinations(2).toList()
-        )
-        assertEquals(
-            listOf(
-                listOf(1, 2),
-                listOf(1, 3),
-                listOf(2, 3),
-            ),
-            listOf(1, 2, 3).combinations(2).toList()
-        )
-        assertEquals(
-            listOf(
-                listOf(1, 2),
-                listOf(1, 3),
-                listOf(1, 4),
-                listOf(2, 3),
-                listOf(2, 4),
-                listOf(3, 4),
-            ),
-            listOf(1, 2, 3, 4).combinations(2).toList()
-        )
+    fun `simple combinations of 2 elements`() {
+        "a".combinations(2).toList().shouldBeEmpty()
+        "ab".combinations(2).toList() shouldContainExactlyInAnyOrder
+                listOf(
+                    "ab"
+                )
+        "abc".combinations(2).toList() shouldContainExactlyInAnyOrder
+                listOf(
+                    "ab",
+                    "ac",
+                    "bc",
+                )
+        "abcd".combinations(2).toList() shouldContainExactlyInAnyOrder
+                listOf(
+                    "ab",
+                    "ac",
+                    "ad",
+                    "bc",
+                    "bd",
+                    "cd",
+                )
     }
 
+    @Test
+    fun `test permutations of elements`() {
+        "".permutations().toList().shouldBeEmpty()
+        "a".permutations().toList() shouldContainExactlyInAnyOrder
+                listOf("a")
+        "ab".permutations().toList() shouldContainExactlyInAnyOrder
+                listOf("ab", "ba")
+        "abc".permutations().toList() shouldContainExactlyInAnyOrder
+                listOf(
+                    "abc",
+                    "acb",
+                    "bac",
+                    "cab",
+                    "bca",
+                    "cba"
+                )
+    }
 
 }
