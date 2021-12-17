6046aa07dbc8a35e18bcf3fcc67eee13cf7f2155
Olaf Gottschalk <Zordid@users.noreply.github.com>
2021-12-17 07:30:42 +0100

    Clean up & speed up day 17

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,114 +1,49 @@
 import utils.*
+import kotlin.math.sign
 
-class Day17 : Day(17, 2021) {
+class Day17 : Day(17, 2021, "Trick Shot") {
 
-    val p = mappedInput { it.extractAllIntegers() }.first()
-    val targetX = p[0]..p[1]
-    val targetY = p[2]..p[3]
+    private val ints = mappedInput { it.extractAllIntegers() }.first()
+    private val targetX = ints[0]..ints[1]
+    private val targetY = ints[2]..ints[3]
 
-    fun Int.drag() = when (this) {
-        0 -> 0
-        in 1..Int.MAX_VALUE -> this - 1
-        else -> this + 1
+    private fun maxHeight(vi: Point): Int? {
+        var p = origin
+        var v = vi
+        var max = 0
+        while (p.x <= targetX.last && p.y >= targetY.first) {
+            if (p.x in targetX && p.y in targetY)
+                return max
+            if (v.y > -targetY.last * 2)
+                return null
+            p += v
+            max = maxOf(max, p.y)
+            v = (v.x - v.x.sign to v.y - 1)
+        }
+        return null
     }
 
     override fun part1(): Any {
-        return 0
-
-        fun maxHeight(vi: Point): Triple<Int?, Boolean, Boolean> {
-            var p = origin
-            var v = vi
-            var max: Int = 0
-            while (p.x <= targetX.last && p.y >= targetY.first) {
-                //println(p)
-                if (p.x in targetX && p.y in targetY) {
-                    return Triple(max, true, true)
-                }
-                p += v
-                max = maxOf(max, p.y)
-                v = (v.x.drag() to v.y - 1)
-            }
-            return Triple(null,
-                p.y < targetY.first,
-                (v.x == 0 && p.y + v.y < targetY.first) || p.x > targetX.last)
-        }
-
-//        println(maxHeight(7 to 2))
-//        println(maxHeight(6 to 3))
-//        println(maxHeight(9 to 0))
-//        println(maxHeight(17 to -4))
-        //  println(maxHeight(7 to 13000))
-        //  return 0
-
-
         var max = 0
         for (vx in targetX.last downTo 1) {
             //for (vy in targetY.first..Int.MAX_VALUE) {
             for (vy in -10000..10000) {
-                //print("$vx $vy - ")
-                val (m, tooLow, tooHigh) = maxHeight(vx to vy)
-//                if (m == null) {
-//                    println("tooLow: $tooLow tooHigh: $tooHigh")
-//                    if (tooHigh) break
-//                    if (tooLow) continue
-//                }
+                val m = maxHeight(vx to vy)
                 if (m != null && m > max) max = m
-                //println(" - $m - $max")
             }
         }
-
-
         return max
     }
 
     override fun part2(): Any {
-
-
-        fun maxHeight(vi: Point): Triple<Int?, Boolean, Boolean> {
-            var p = origin
-            var v = vi
-            var max: Int = 0
-            while (p.x <= targetX.last && p.y >= targetY.first) {
-                //println(p)
-                if (p.x in targetX && p.y in targetY) {
-                    return Triple(max, true, true)
-                }
-                p += v
-                max = maxOf(max, p.y)
-                v = (v.x.drag() to v.y - 1)
-            }
-            return Triple(null,
-                p.y < targetY.first,
-                (v.x == 0 && p.y + v.y < targetY.first) || p.x > targetX.last)
-        }
-
-//        println(maxHeight(7 to 2))
-//        println(maxHeight(6 to 3))
-//        println(maxHeight(9 to 0))
-//        println(maxHeight(17 to -4))
-        //  println(maxHeight(7 to 13000))
-        //  return 0
-
-
-        var max = 0
         var count = 0
         for (vx in targetX.last downTo 1) {
             //for (vy in targetY.first..Int.MAX_VALUE) {
             for (vy in -10000..10000) {
-                //print("$vx $vy - ")
-                val (m, tooLow, tooHigh) = maxHeight(vx to vy)
-//                if (m == null) {
-//                    println("tooLow: $tooLow tooHigh: $tooHigh")
-//                    if (tooHigh) break
-//                    if (tooLow) continue
-//                }
+                val m = maxHeight(vx to vy)
                 if (m != null) count++
-                if (m != null && m > max) max = m
-                //println(" - $m - $max")
             }
         }
-
-
         return count
     }
 
@@ -117,5 +52,5 @@ class Day17 : Day(17, 2021) {
 fun main() {
     solve<Day17>("""
         target area: x=20..30, y=-10..-5
-    """.trimIndent(), null, 112)
+    """.trimIndent(), 45, 112)
 }
--- src/test/kotlin/DaysTest.kt
+++ src/test/kotlin/DaysTest.kt
@@ -5,24 +5,25 @@ import org.junit.jupiter.api.TestFactory
 class DaysTest {
 
     @TestFactory
-    fun all(): List<DynamicTest> = tests(
-        test<Day01>(1298, 1248),
-        test<Day02>(1580000, 1251263225),
-        test<Day03>(1458194, 2829354),
-        test<Day04>(28082, 8224),
-        test<Day05>(6283, 18864),
-        test<Day06>(372300, 1675781200288),
-        test<Day07>(337488, 89647695),
-        test<Day08>(239, 946346),
-        test<Day09>(512, 1600104),
-        test<Day10>(344193, 3241238967),
-        test<Day11>(1747, 505),
-        test<Day12>(5212, 134862),
-        test<Day13>(592, DAY13PT2),
-        test<Day14>(3555, 4439442043739),
-        test<Day15>(755, 3016),
-        test<Day16>(938, 1495959086337),
-    )
+    fun `AoC 2021`() = aocTests {
+        test<Day01>(1298, 1248)
+        test<Day02>(1580000, 1251263225)
+        test<Day03>(1458194, 2829354)
+        test<Day04>(28082, 8224)
+        test<Day05>(6283, 18864)
+        test<Day06>(372300, 1675781200288)
+        test<Day07>(337488, 89647695)
+        test<Day08>(239, 946346)
+        test<Day09>(512, 1600104)
+        test<Day10>(344193, 3241238967)
+        test<Day11>(1747, 505)
+        test<Day12>(5212, 134862)
+        test<Day13>(592, DAY13PT2)
+        test<Day14>(3555, 4439442043739)
+        test<Day15>(755, 3016)
+        test<Day16>(938, 1495959086337)
+        test<Day17>(11175, 3540)
+    }
 
     companion object {
         val DAY13PT2 = """
@@ -36,14 +37,26 @@ class DaysTest {
     }
 }
 
-private fun tests(vararg d: List<DynamicTest>) = d.asList().flatten().also { verbose = false }
+private fun aocTests(builder: AoCTestBuilder.() -> Unit): List<DynamicTest> =
+    AoCTestBuilder().apply(builder).build().also { verbose = false }
 
-private inline fun <reified D : Day> test(expectedPart1: Any? = null, expectedPart2: Any? = null) = listOfNotNull(
-    expectedPart1?.let {
-        DynamicTest.dynamicTest("${D::class.simpleName} - Part 1")
-        { create<D>().part1.toString() shouldBe expectedPart1.toString() }
-    },
-    expectedPart2?.let {
-        DynamicTest.dynamicTest("${D::class.simpleName} - Part 2")
-        { create<D>().part2.toString() shouldBe expectedPart2.toString() }
-    })
+private class AoCTestBuilder {
+
+    private val tests = mutableListOf<DynamicTest>()
+
+    inline fun <reified D : Day> test(expectedPart1: Any? = null, expectedPart2: Any? = null) {
+        tests += listOfNotNull(
+            expectedPart1?.let {
+                DynamicTest.dynamicTest("${D::class.simpleName} - Part 1")
+                { create<D>().part1.toString() shouldBe expectedPart1.toString() }
+            },
+            expectedPart2?.let {
+                DynamicTest.dynamicTest("${D::class.simpleName} - Part 2")
+                { create<D>().part2.toString() shouldBe expectedPart2.toString() }
+            })
+    }
+
+    fun build(): List<DynamicTest> = tests
+
+}
+
